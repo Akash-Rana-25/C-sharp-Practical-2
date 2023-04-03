754ad5ba4c72e73c484e23aff08eec9344c6f804
Akash-Rana-25 <akashrana.work.gmail.com>
2023-04-03 14:48:03 +0530

    Bank App-Practical-2

---
+++ Bank_practical_2/Bank_practical_2/Customer_Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_practical_2
+{
+    class Customer_Account
+    {
+        string bank_name = "SBI";
+        long customer_accountNo;
+        string customer_name;
+
+        public Customer_Account(long acc_no, string c_name)
+        {
+
+            customer_accountNo = acc_no;
+            customer_name = c_name;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Bank Name:" + bank_name);
+            Console.WriteLine("Customer Name:" + customer_name);
+            Console.WriteLine("cusomer Account no :" + customer_accountNo);
+
+        }
+    }
+}
---
+++ Bank_practical_2/Bank_practical_2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bank_practical_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer_Account customer_info = new(123,"Akash");
+            customer_info.PrintInfo();
+        }
+    }
+  
+}
